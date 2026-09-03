de518fde7d3c33dd7cf55ebe3e054e085dc2da2e
Mohamed <mookhaleddd@gmail.com>
2026-09-03 07:51:53 +0300

    add custom error page and fix routing

--- front-end/src/route/index.jsx
+++ front-end/src/route/index.jsx
@@ -142,6 +142,10 @@ const router = createBrowserRouter([
         path: "cancel",
         element: <Cancel />,
       },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
     ],
   },
 ]);
