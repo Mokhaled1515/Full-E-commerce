3803f6e31f72a7b950905494de01f93fa73491b4
Mohamed <mookhaleddd@gmail.com>
2026-09-03 07:07:29 +0300

    validURLConvert

--- front-end/src/utils/ValidUrlConverter.js
+++ front-end/src/utils/ValidUrlConverter.js
@@ -1,8 +1,30 @@
-export const validURLConvert = (name) =>{
-    if(!name){
-        return ""
-    }
- const url = name?.toString().replaceAll(" ","-").replaceAll(",","-").replaceAll("&","-")
- return url
-}
+// export const validURLConvert = (name) =>{
+//     if(!name){
+//         return ""
+//     }
+//  const url = name?.toString().replaceAll(" ","-").replaceAll(",","-").replaceAll("&","-")
+//  return url
+// }
 
+export const validURLConvert = (name) => {
+  if (!name) {
+    return "";
+  }
+
+  return (
+    name
+      .toString()
+      .trim()
+      .toLowerCase()
+      // Replace slash and backslash
+      .replace(/[\/\\]+/g, "-")
+      // Replace special characters
+      .replace(/[^a-z0-9\s-]/g, "")
+      // Replace spaces with -
+      .replace(/\s+/g, "-")
+      // Remove duplicate -
+      .replace(/-+/g, "-")
+      // Remove - from beginning/end
+      .replace(/^-+|-+$/g, "")
+  );
+};
